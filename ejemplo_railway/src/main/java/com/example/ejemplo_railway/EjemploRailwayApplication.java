e081e8f1b83daf211d8d8e35f63177f7b4b3fed3
Fabian Parraguez <fabi.parraguez@duocuc.cl>
2025-06-14 09:21:07 -0400

    Add files via upload

---
+++ ejemplo_railway/src/main/java/com/example/ejemplo_railway/EjemploRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploRailwayApplication.class, args);
+	}
+
+}
