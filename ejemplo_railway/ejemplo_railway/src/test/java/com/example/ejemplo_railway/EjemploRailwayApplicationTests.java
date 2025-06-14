fc6d262c2495c2101481813f6e007c6ecf5e8d70
Fabian Parraguez <fabi.parraguez@duocuc.cl>
2025-06-14 09:29:50 -0400

    Add files via upload

---
+++ ejemplo_railway/ejemplo_railway/src/test/java/com/example/ejemplo_railway/EjemploRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
